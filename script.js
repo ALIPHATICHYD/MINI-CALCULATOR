cabdb2fe11814561e990528271eac173fea040ee
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2022-07-02 17:31:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const firstNum = document.getElementById('1st');
 
+const secondNum = document.getElementById('2nd');
+
+const multiplyBtn = document.getElementById('multiply');
+
+const divideBtn = document.getElementById('divide');
+
+const resultOut = document.getElementById('ans')
